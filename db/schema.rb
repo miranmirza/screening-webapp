7bfeabbb882fce6e82b41a2cc724956692855409
Emily Berkeley <eberkeley@gmail.com>
2015-06-06 13:31:20 -0600

    Move app and db dirs

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150606164633) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "city"
+    t.string   "state"
+    t.string   "skype"
+    t.string   "hangout"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "interview_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "comments", ["interview_id"], name: "index_comments_on_interview_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "interviews", force: :cascade do |t|
+    t.integer  "candidate_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "interviewscreeners", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "interview_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "interviewscreeners", ["interview_id"], name: "index_interviewscreeners_on_interview_id", using: :btree
+  add_index "interviewscreeners", ["user_id"], name: "index_interviewscreeners_on_user_id", using: :btree
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "archived",   default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "complete",          default: false
+    t.string   "description"
+    t.date     "due_date"
+    t.integer  "list_id"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "skype"
+    t.string   "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "comments", "interviews"
+  add_foreign_key "comments", "users"
+  add_foreign_key "interviewscreeners", "interviews"
+  add_foreign_key "interviewscreeners", "users"
 end
