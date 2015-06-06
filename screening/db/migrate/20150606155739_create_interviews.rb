dba6e8be79977333d5f9bcfca920297f45a3dbdf
Miran Mirza <miranmirza@gmail.com>
2015-06-06 12:47:13 -0400

    Database setup
    Finished migrations and model set up

--- screening/db/migrate/20150606155739_create_interviews.rb
+++ screening/db/migrate/20150606155739_create_interviews.rb
@@ -2,7 +2,7 @@ class CreateInterviews < ActiveRecord::Migration
   def change
     create_table :interviews do |t|
       t.string :comments
-      t.reference :candidate
+      t.references :candidate
 
       t.timestamps null: false
     end
