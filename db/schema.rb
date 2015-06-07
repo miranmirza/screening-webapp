b4277150ab2732bf6fc762e672d5885a9d48f8e5
Sai Warang <cyprusad@gmail.com>
2015-06-07 10:31:29 -0400

    Add migration and view logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606214832) do
+ActiveRecord::Schema.define(version: 20150607033341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 20150606214832) do
     t.string   "state"
     t.string   "skype"
     t.string   "hangout"
-    t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "status",     default: "pending"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
   create_table "comments", force: :cascade do |t|
