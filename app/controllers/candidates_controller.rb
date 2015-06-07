cdd68e4dc2a8ccd6aceda4f22f9ccdfc235ae527
Emily Berkeley <eberkeley@gmail.com>
2015-06-07 10:20:46 -0600

    stuff

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,7 +2,7 @@ class CandidatesController < ApplicationController
   def index
     @pending_candidates = Candidate.all.pending
     @approved_candidates = Candidate.where(status: "approved")
-    # TODO @denied_candidates = Candidate.where(status: "denied")
+    #TODO @denied_candidates = Candidate.where(status: "denied")
   end
 
   def show
